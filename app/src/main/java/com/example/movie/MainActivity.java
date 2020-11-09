ebee7e114460d2c66fdef026ecc3ab71ff883303
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2020-11-10 02:38:53 +0900

    코드 정리

--- app/src/main/java/com/example/movie/MainActivity.java
+++ app/src/main/java/com/example/movie/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.container);
